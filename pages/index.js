e5d5cd6fef51f113cf9c846a074f0a829fc17b53
2917HELDER <107595631+2917HELDER@users.noreply.github.com>
2025-03-04 22:36:10 +0000

    adiciona ascripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-    return <h1>Oi Amor se vc me Ama da um sorrisinho❤️😄</h1>
-  
+function Home() {
+  return <h1>Oi Amor se vc me Ama da um sorrisinho❤️😄</h1>;
 }
 
 export default Home;
